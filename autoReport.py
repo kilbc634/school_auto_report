1937f31881d39e72bf2806249cb8fd57acbbc240
kilbc634 <tuyn76801@gmail.com>
2020-03-31 13:11:08 +0800

    add template ini file and complated today report function

--- autoReport.py
+++ autoReport.py
@@ -2,6 +2,11 @@ import requests
 import json
 import time
 from configparser import ConfigParser
+import datetime
+
+WEEKS_STR = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+maxBackDay = 7
+warningTemperature = 37.5
 
 class requestLib():
     def __init__(self):
@@ -51,22 +56,82 @@ class requestLib():
         resp = self.__request('get', 'departments')
         return resp
 
-    def post_tempData(self, token, userId, departmentId, departmentName, className,
-        morningTemp=34, morningActivity,
-        noonTemp=37.5, noonActivity,
-        nightTemp=34, nightActivity)
-
-
+    def post_tempData(self, token, userId, departmentId, departmentName, className, date,
+        morningTemp=34, morningActivity='',
+        noonTemp=34, noonActivity='',
+        nightTemp=34, nightActivity='',
+        method='post'):
+        self.SESSION.headers.update({'authorization': 'Bearer %s' % token})
+        data = {
+            "id": userId + "-undefined",
+            "saveDate": date,
+            "morningTemp": morningTemp,
+            "noonTemp": noonTemp,
+            "nightTemp": nightTemp,
+            "isValid": False,
+            "morningManner": 0,
+            "noonManner": 0,
+            "nightManner": 0,
+            "isMorningFever": None,
+            "isNoonFever": False,
+            "isNightFever": None,
+            "morningActivity": morningActivity,
+            "noonActivity": noonActivity,
+            "nightActivity": nightActivity,
+            "measureTime": "18:00",
+            "userId": userId,
+            "departmentId": departmentId,
+            "className": className,
+            "departmentName": departmentName,
+            "type": "1"
+        }
+        resp = self.__request(method, 'temperatureSurveys', json=data)
+        if resp.status_code == 200:
+            respJson = json.loads(resp.text)
+            return respJson
+        elif resp.status_code == 500:
+            respJson = {
+                "success": False,
+                "messages": [
+                    "當天資料已存在"
+                ]
+            }
+            return respJson
+        else:
+            resp.raise_for_status()
 
 if __name__ == "__main__":
     Lib = requestLib()
-    config = ConfigParser()
-    config.read('user.ini')
-    myAccount = config['user']['account']
-    myPassword = config['user']['password']
+
+    user = ConfigParser()
+    user.read('user.ini')
+
+    envId = user['env']['departmentId']
+    envName = user['env']['departmentName']
+    className = user['env']['className']
+
+    myAccount = user['user']['account']
+    myPassword = user['user']['password']
     if myAccount.find('(') == 0 or myPassword.find('(') == 0:
         print('請登入系統（預設帳號密碼同學生篇），或於 user.ini 檔案設置基本資料')
         myAccount = input('學號:')
         myPassword = input('密碼:')
+
     userToken = Lib.getToken(myAccount, myPassword)
 
+    nowDate = datetime.date.today()
+    nowWeek = nowDate.weekday()    #Output will is 0 = Monday ... 6 = Sunday
+
+    template = ConfigParser()
+    template.read('template.ini')
+    morningDo = template[WEEKS_STR[nowWeek]]['morning']
+    noonDo = template[WEEKS_STR[nowWeek]]['noon']
+    nightDo = template[WEEKS_STR[nowWeek]]['night']
+
+    res = Lib.post_tempData(userToken, myAccount, envId, envName, className, str(nowDate),
+        morningActivity=morningDo, noonActivity=noonDo, nightActivity=nightDo)
+    if res['success'] == False:
+        print('[Error] {msg}'.format(msg=res['messages'][0]))
+        exit()
+
+    #if recordDay != 0:
